4afc7883908599b714f643eb5132e01fb88ee390
huynhtri998 <huynhtri998@gmail.com>
2025-11-18 19:17:09 +0700

    initialize ecm-customer

---
+++ src/main/java/com/trilabs94/ecm_customer/EcmCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.trilabs94.ecm_customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trilabs94/ecm_customer/EcmCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilabs94.ecm_customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
